f6ee5bf6c73ba257086c83bb93286b7c9b9d2d23
rahulsain3000 <50570677+rahulsain@users.noreply.github.com>
2021-03-26 08:15:19 +0530

    load svg file

---
+++ app/src/main/java/com/rahuls/myarea/Continent.java
@@ -0,0 +1,98 @@
+package com.rahuls.myarea;
+
+public class Continent {
+    int id;
+    String countryName;
+    String capital,flagURL,region,subRegion;
+    String population;
+    String borders,languages;
+
+    public Continent(){
+
+    }
+
+    public Continent(int id,String countryName, String capital , String flagURL, String region , String subRegion
+            , String population , String borders , String languages){
+        this.id = id;
+        this.countryName = countryName;
+        this.capital = capital;
+        this.flagURL = flagURL;
+        this.region = region;
+        this.subRegion = subRegion;
+        this.population = population;
+        this.borders = borders;
+        this.languages = languages;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+
+    public String getCapital() {
+        return capital;
+    }
+
+    public void setCapital(String capital) {
+        this.capital = capital;
+    }
+
+    public String getFlagURL() {
+        return flagURL;
+    }
+
+    public void setFlagURL(String flagURL) {
+        this.flagURL = flagURL;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public String getSubRegion() {
+        return subRegion;
+    }
+
+    public void setSubRegion(String subRegion) {
+        this.subRegion = subRegion;
+    }
+
+    public String getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(String population) {
+        this.population = population;
+    }
+
+    public String getBorders() {
+        return borders;
+    }
+
+    public void setBorders(String borders) {
+        this.borders = borders;
+    }
+
+    public String getLanguages() {
+        return languages;
+    }
+
+    public void setLanguages(String languages) {
+        this.languages = languages;
+    }
+}
---
+++ app/src/main/java/com/rahuls/myarea/MyApplication.java
@@ -0,0 +1,25 @@
+package com.rahuls.myarea;
+
+import android.app.Application;
+import android.content.Context;
+
+public class MyApplication extends Application {
+
+    private static MyApplication sInstance;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        sInstance=this;
+    }
+
+    public static MyApplication getInstance() {
+        return sInstance;
+    }
+
+    public Context getAppContext() {
+        return sInstance.getApplicationContext();
+    }
+
+
+}
